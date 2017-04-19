febae445536e98afe43aed0a37a855147d924d4b
Daniil <vereshhagin98@mail.ru>
2017-04-19 10:27:41 +0400

    add client render on localhost

--- client/main.js
+++ client/main.js
@@ -1,5 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
+//  import React from 'react'
+//  import ReactDOM from 'react-dom';
+
+var Notes = React.createClass({
+    render:function(){
+        return(
+        <h1>Notes</h1>
+        )
+    }
+
+})
+
 ReactDOM.render(
     <h1>Notes</h1>,
     document.getElementById('mount-point')
